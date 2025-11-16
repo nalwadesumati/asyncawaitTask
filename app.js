a4d874c8d42b0e4e1ea4d7d6484c76cb7ee1bf3e
nalwadesumati <165995604+nalwadesumati@users.noreply.github.com>
2025-11-16 22:06:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
 const cl = console.log;
-const blogForm = document.getElementById("blogForm")
 
-const blogTitlectrl = document.getElementById("blogTitle")
-const blogContentctrl = document.getElementById("blogContent")
+const blogForm = document.getElementById("blogForm");
 
+const blogTitlectrl = document.getElementById("blogTitle");
+const blogContentctrl = document.getElementById("blogContent");
 
-const blogContainer = document.getElementById("blogContainer")
-
-
+const blogContainer = document.getElementById("blogContainer");
 
 const uuid = () => {
     return String("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx").replace(/[xy]/g, (character) => {
         const random = (Math.random() * 16) | 0;
         const value = character === "x" ? random : (random & 0x3) | 0x8;
         return value.toString(16);
-    })
+    });
 };
-
-
 
 const snackbar = (icon, msg) => {
     Swal.fire({
         icon: icon,
         title: msg,
         timer: 1000
-    })
-}
+    });
+};
 
 
-let blogArr = [{
-    title: "Javascript",
-    content: "JavaScript is a programming language and core technology of the Web, alongside HTML and CSS.It was created by Brendan Eich in 1995. Ninety - nine percent of websites use JavaScript on the client side for webpage behavior."
-},
-{
-    title: "HTML",
-    content: "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content."
-}];
-
+let blogArr = [
+    {
+        title: "Javascript",
+        content: "JavaScript is a programming language..."
+    },
+    {
+        title: "HTML",
+        content: "Hypertext Markup Language is the standard markup language..."
+    }
+];
 
 
 const createBlogs = (data) => {
     let result = "";
     data.forEach(blog => {
         result += `
-        <div class="row mt-2" id="blogContainer">
+        <div class="row mt-2">
             <div class="col-md-3 col-sm-6 text-center">
                 <div class="card">
                     <div class="card-header">
@@ -52,95 +49,80 @@ const createBlogs = (data) => {
                     <div class="card-body">
                         <p>${blog.content}</p>
                     </div>
-                     <button class="btn btn-sm btn-outline-primary btn-block mt-5">Add Blog</button>
-                            <button class="btn btn-sm btn-outline-primary btn-block mt-5 d-none">Update Blog</button>
                 </div>
-            </div>`
-    })
+            </div>
+        </div>`;
+    });
+
     blogContainer.innerHTML = result;
-}
+};
+
 createBlogs(blogArr);
 
 
-
-
 async function onBlogSubmit(eve) {
-    eve.preventDefault()
+    eve.preventDefault();
 
     let blogObj = {
         title: blogTitlectrl.value,
         content: blogContentctrl.value
-    }
-    cl(blogObj)
+    };
+
     eve.target.reset();
 
     try {
-        let res = await postBlog(blogObj)
-        cl(res)
-        snackbar('success', 'New blog Created Successfully!!!!')
-        let data = await featchBlog(blogObj)
-        cl(data)
-        createBlogs(data.blog);
-        snackbar('success', 'New blog Created Successfully!!!!')
+        let res = await postBlog(blogObj);
+        snackbar('success', 'New blog created successfully!');
+
+        let data = await featchBlog();
+        createBlogs(data.blogs);
 
     } catch (err) {
-        snackbar('error', 'something went wrong while creating a new blog!!!')
+        snackbar('error', 'Something went wrong while creating a blog!');
     }
-
-
 }
+
 
 async function postBlog(obj) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             let err = Math.random() > .3 ? false : true;
-            if (!err) {
-                blogArr.push(obj)
-                resolve({
-                    msg: 'New blog created Successfully!!!!!',
-                    blog: { ...obj, id: uuid() }
-                })
 
+            if (!err) {
+                blogArr.push(obj);
+                resolve({
+                    msg: "New blog created successfully!",
+                    blog: { ...obj, id: uuid() }
+                });
             } else {
                 reject({
-                    msg: `Something went wrong while creating new blog!!!!`,
-                    stetusCode: 500
-                })
+                    msg: "Error creating blog!",
+                    statusCode: 500
+                });
             }
-        }, 800)
-    })
+        }, 800);
+    });
 }
-async function featchBlog(obj) {
+
+
+async function featchBlog() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             let err = Math.random() > .3 ? false : true;
-            if (!err) {
-                blogArr.push(obj)
-                resolve({
-                    msg: 'New blog featched Successfully!!!!!',
-                    blog: { ...obj, id: uuid() }
-                })
 
+            if (!err) {
+                resolve({
+                    msg: "Blogs fetched successfully!",
+                    blogs: blogArr
+                });
             } else {
                 reject({
-                    msg: `Something went wrong while featching new blog!!!!`,
-                    stetusCode: 500
-                })
+                    msg: "Error fetching blogs!",
+                    statusCode: 500
+                });
             }
-        }, 800)
-    })
+        }, 800);
+    });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-blogForm.addEventListener("submit", onBlogSubmit)
+blogForm.addEventListener("submit", onBlogSubmit);
