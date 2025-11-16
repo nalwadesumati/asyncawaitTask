d1c3a512b420c62bff5741743903bca1c09d3c3d
nalwadesumati <165995604+nalwadesumati@users.noreply.github.com>
2025-11-16 22:31:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,128 +1,120 @@
-const cl = console.log;
-
-const blogForm = document.getElementById("blogForm");
-
-const blogTitlectrl = document.getElementById("blogTitle");
-const blogContentctrl = document.getElementById("blogContent");
-
-const blogContainer = document.getElementById("blogContainer");
-
-const uuid = () => {
-    return String("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx").replace(/[xy]/g, (character) => {
-        const random = (Math.random() * 16) | 0;
-        const value = character === "x" ? random : (random & 0x3) | 0x8;
-        return value.toString(16);
-    });
-};
-
-const snackbar = (icon, msg) => {
-    Swal.fire({
-        icon: icon,
-        title: msg,
-        timer: 1000
-    });
-};
-
-
-let blogArr = [
+let blogsArr = [
     {
-        title: "Javascript",
-        content: "JavaScript is a programming language..."
+        blogTitle: 'Async-await',
+        blogContent: `async and await are modern JavaScript keywords that make working with promises easier and cleaner.
+Instead of writing messy .then() chains, you can write asynchronous code that looks synchronous.`
     },
     {
-        title: "HTML",
-        content: "Hypertext Markup Language is the standard markup language..."
+        blogTitle: 'Angular',
+        blogContent: `Angular is a TypeScript-based front-end framework by Google for building fast, scalable single-page web applications.`
     }
 ];
 
 
-const createBlogs = (data) => {
+const blogForm = document.getElementById("blogForm");
+const blogTitlectrl = document.getElementById("blogTitle");
+const blogContentctrl = document.getElementById("blogContent");
+const blogContainer = document.getElementById("blogContainer");
+
+const snackBar = (title, icon = "success") => {
+    Swal.fire({
+        title,
+        icon,
+        timer: 1500
+    });
+};
+
+function onBlogSubmit(e) {
+    e.preventDefault();
+
+    let blogObj = {
+        blogTitle: blogTitlectrl.value,
+        blogContent: blogContentctrl.value
+    };
+
+    postBlog(blogObj)
+        .then(res => {
+            snackBar(res.msg);
+            return fetchBlog();
+        })
+        .then(res => {
+            createBlogs(res.blog);
+        })
+        .catch(err => {
+            snackBar(err.msg, "error");
+        });
+
+    blogForm.reset();
+}
+
+function postBlog(blog) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err = Math.random() > .1 ? false : true;
+
+            if (!err) {
+                blogsArr.unshift(blog);
+                resolve({
+                    msg: `New blog added successfully!`,
+                    blogObj: { ...blog, id: "fromBE" }
+                });
+            } else {
+                reject({
+                    msg: `Something went wrong while creating a new blog`,
+                    statusCode: 400
+                });
+            }
+        }, 1000);
+    });
+}
+
+
+function fetchBlog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err = Math.random() > .1 ? false : true;
+
+            if (!err) {
+                resolve({
+                    msg: `All blogs fetched successfully`,
+                    blog: blogsArr
+                });
+            } else {
+                reject({
+                    msg: `Something went wrong while fetching blogs`,
+                    statusCode: 400
+                });
+            }
+        }, 500);
+    });
+}
+
+
+function createBlogs(data) {
     let result = "";
+
     data.forEach(blog => {
         result += `
-        <div class="row mt-2">
-            <div class="col-md-3 col-sm-6 text-center">
+            <div class="col-md-3 col-sm-6 mb-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5>${blog.title}</h5>
+                        <h5>${blog.blogTitle}</h5>
                     </div>
                     <div class="card-body">
-                        <p>${blog.content}</p>
+                        <p>${blog.blogContent}</p>
                     </div>
+
+                    <button class="btn btn-outline-primary btn-sm btn-block mt-3">Edit</button>
+                    <button class="btn btn-outline-danger btn-sm btn-block mt-2">Remove</button>
                 </div>
             </div>
-        </div>`;
+        `;
     });
 
     blogContainer.innerHTML = result;
-};
-
-createBlogs(blogArr);
-
-
-async function onBlogSubmit(eve) {
-    eve.preventDefault();
-
-    let blogObj = {
-        title: blogTitlectrl.value,
-        content: blogContentctrl.value
-    };
-
-    eve.target.reset();
-
-    try {
-        let res = await postBlog(blogObj);
-        snackbar('success', 'New blog created successfully!');
-
-        let data = await featchBlog();
-        createBlogs(data.blogs);
-
-    } catch (err) {
-        snackbar('error', 'Something went wrong while creating a blog!');
-    }
 }
 
 
-async function postBlog(obj) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let err = Math.random() > .3 ? false : true;
-
-            if (!err) {
-                blogArr.push(obj);
-                resolve({
-                    msg: "New blog created successfully!",
-                    blog: { ...obj, id: uuid() }
-                });
-            } else {
-                reject({
-                    msg: "Error creating blog!",
-                    statusCode: 500
-                });
-            }
-        }, 800);
-    });
-}
-
-
-async function featchBlog() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let err = Math.random() > .3 ? false : true;
-
-            if (!err) {
-                resolve({
-                    msg: "Blogs fetched successfully!",
-                    blogs: blogArr
-                });
-            } else {
-                reject({
-                    msg: "Error fetching blogs!",
-                    statusCode: 500
-                });
-            }
-        }, 800);
-    });
-}
+createBlogs(blogsArr);
 
 blogForm.addEventListener("submit", onBlogSubmit);
